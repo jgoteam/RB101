0d0e34571099e5c9c67ab7d9e68e814b7f8de8ac
jgoteam <j3liang@icloud.com>
2022-11-17 11:42:59 -0800

    11-27-22

--- lesson_3/factor_methods.rb
+++ lesson_3/factor_methods.rb
@@ -9,16 +9,19 @@ def factors(number)
 end
 
 def factors2(num)
-    (1..num).select{ |x| (num % x == 0)} if num != 0
+  num.zero? ? [0] : (1..num).select { |x| (num % x == 0)}
 end
 
 def factors3(num)
   divisor = num
   factors = []
   while divisor > 0
-    factors << number / divisor if number % divisor == 0
-    divisor =- 1
+    factors << num / divisor if num % divisor == 0
+    divisor -= 1
   end
+  factors
 end
 
+p factors(1)
+p factors2(0)
 p factors3(0)
