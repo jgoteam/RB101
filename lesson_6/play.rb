2f8eaa1366f6bd5bf4cdff00d7cb40cf8776cb4f
jgoteam <j3liang@icloud.com>
2022-11-29 17:32:58 -0800

    Added 21_gameV1

--- lesson_6/play.rb
+++ lesson_6/play.rb
@@ -1,17 +1,49 @@
 
+suits = ['♠', '♥', '♦', '♣']
+possible_cards = ('2'..'10').to_a + ['J', 'Q', 'K', 'A']
 
-def joinor(num_arr, delimiter = ', ', append_last = 'or')
-  if num_arr.size == 1 || num_arr.size == 0
-    num_arr.join
-  elsif num_arr.size == 2
-    "#{num_arr[0]} #{append_last} #{num_arr[1]}"
-  else
-    num_arr.map.with_index { |i, idx| idx != num_arr.size - 1 ? "#{i}#{delimiter}" : "#{append_last} #{i}" }.join
-  end
+new_deck = ((possible_cards)*4).zip(suits*13).shuffle
+
+CARD_VALUES = (possible_cards).each_with_object({}) do |card, new_hash|
+                (2..10).include?(card.to_i)? new_hash[card] = card.to_i : card == 'Ace' ? new_hash[card] = [1,11] : new_hash[card] = 10
+              end
+MAX_NO_BUST = 21
+player1 = []
+
+def add_hidden_card!(dealer, deck)
+  dealers_hand << ["?", "?"]
+  mystery_card = deck.pop
 end
 
-p joinor([1])
-p joinor([1, 2])                   # => "1 or 2"
-p joinor([1, 2, 3])                # => "1, 2, or 3"
-p joinor([1, 2, 3], '; ')          # => "1; 2; or 3"
-p joinor([1, 2, 3], ', ', 'and')   # => "1, 2, and 3"
+def deal_card!(player, num_of_cards, deck)
+  dealt_cards = []
+  num_of_cards.times do
+    player << deck.pop
+    dealt_cards << player.last
+  end
+  dealt_cards
+end
+
+def find_total(whos_hand)
+  total = 0
+  whos_hand.each do |card|
+    if card[0] == 'A'
+      total + CARD_VALUES[card[0]][1] > MAX_NO_BUST ? total += CARD_VALUES[card[0]][0] : total += CARD_VALUES[card[0]][1]
+    else
+      total += CARD_VALUES[card[0]]
+    end
+  end
+  total
+end
+
+3.times { player1 << new_deck.pop }
+
+#p player1
+
+p deal_card!(player1, 1, new_deck)[0]
+
+p player1
+p find_total(player1)
+
+
+
